b4980f0b0be42be6ee06b915fc140f85724cc713
RuslanMak <makarovemd@gmail.com>
2019-02-06 12:32:47 +0200

    lesson 9 - done

--- routes/console.php
+++ routes/console.php
@@ -16,3 +16,7 @@ use Illuminate\Foundation\Inspiring;
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->describe('Display an inspiring quote');
+
+Artisan::command('ruslan', function () { /* php artisan ruslan */
+    $this->comment('Hi Ruslan');
+})->describe('Replies to you when called.');
